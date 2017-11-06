b306872b4072aaf44d91a80baeb53dde8a0cf964
mmitacc <manuelmitacc@hotmail.com>
2017-11-05 19:47:22 -0500

    Reconstruyendo con un modelo patron de JavaScript

--- js/app.js
+++ js/app.js
@@ -1,99 +1,99 @@
-var Calculadora = {
-  dato1:"", signo1:"+", dato2:"", signo2:"+",
-  operador:"", resultado:"", OnC:46,
+var Calculadora = (function () {
+  //Declarando variables globales
+  var dato1="", signo1="+", dato2="", signo2="+",
+      operador="", resultado="", OnC=46;
+
   //Método para reducir tamaño de tecla al presionarla
-  PresionarTecla : function (idtecla) {
-    idtecla = idtecla;
+  var PresionarTecla = function (idtecla) {
     var reducirtecla = document.getElementById(idtecla);
     reducirtecla.style.background= "#bdbdbd";
     reducirtecla.style.border= "1px solid #000000";
     reducirtecla.style.borderRadius="15px";
     reducirtecla.style.padding= "8px";
-  },
+  };
   //Método para mantener tamaño original de la tecla al soltarla
-  SoltarTecla : function (event) {
+  var SoltarTecla = function (event) {
     var tecla = event.which || event.keyCode;
-    if (tecla != this.OnC) {
-      /*var datovalidado = Calculadora.ValidarDato(tecla);
+    if (tecla != OnC) {
+      var datovalidado = ValidarDato(tecla);
       if (datovalidado != "") {
         var ampliartecla = document.getElementById(datovalidado);
         ampliartecla.style.background= "transparent";
         ampliartecla.style.border= "none";
         ampliartecla.style.padding= "0px";
-      }*/
-    } else {
+      };
+    }
+    else {
       var ampliartecla = document.getElementById("on");
       ampliartecla.style.background= "transparent";
       ampliartecla.style.border= "none";
       ampliartecla.style.padding= "0px";
-      //dato1=""; signo1="+"; dato2=""; signo2="+"; operador=""; resultado="";
     }
-  },
+  };
   //Método para la lectura de los datos
-  IngresoDatos : function (event) {
+  var IngresoDatos = function (event) {
     var tecla = event.which || event.keyCode;
-    if (tecla != this.Onc) {
-       var datovalidado = this.ValidarDato(tecla);
-      /*if (datovalidado != "") {
-        Calculadora.PresionarTecla(datovalidado);
+    if (tecla != OnC) {
+       var datovalidado = ValidarDato(tecla);
+       if (datovalidado != "") {
+        PresionarTecla(datovalidado);
         //AGREGAR ACUMULADORES PARA LAS OPERACIONES Y MOSTAR EN DISPLAY
         document.getElementById("display").innerHTML=String.fromCharCode(tecla);
-      };*/
+      };
     } else {
-      this.PresionarTecla("on");
+      PresionarTecla("on");
       document.getElementById("display").innerHTML="borrados";
       dato1=""; signo1="+"; dato2=""; signo2="+"; operador=""; resultado="";
     };
     //Visualiza el dato en el DISPLAY
-  },
+  };
   //Método para validar si el dato es NUMERO ó es un OPERADOR
-  ValidarDato : function (dato) {
-    var dato=dato;
+  var ValidarDato = function (dato) {
     var TipoDato="";
     switch (dato) {
-      case 96:
+      case 48:
         TipoDato="0";
         break;
-      case 97:
+      case 49:
         TipoDato="1";
         break;
-      case 98:
+      case 50:
         TipoDato="2";
         break;
-      case 99:
+      case 51:
         TipoDato="3";
         break;
-      case 100:
+      case 52:
         TipoDato="4";
         break;
-      case 101:
+      case 53:
         TipoDato="5";
         break;
-      case 102:
+      case 54:
         TipoDato="6";
         break;
-      case 103:
+      case 55:
         TipoDato="7";
         break;
-      case 104:
+      case 56:
         TipoDato="8";
         break;
-      case 105:
+      case 57:
         TipoDato="9";
         break;
-      case 110:
+      case 46:
         TipoDato="punto";
         break;
-      case 107:
+      case 43:
         TipoDato="mas";
         break;
-      case 109:
+      case 45:
         TipoDato="menos";
         break;
-      case 106:
+      case 42:
         TipoDato="por";
         break;
-      case 111:
+      case 47:
         TipoDato="dividido";
         break;
       case 13:
@@ -101,17 +101,17 @@ var Calculadora = {
         break;
       default:
         //alert("No es un dato para la Calculadora");
-        break;
     };
     return TipoDato;
-  },
+  };
   //Inicializando los métodos con eventos de teclado y mouse
-  Return {
-    iniciar : function () {
-      document.onkeypress=this.IngresoDatos;
-      document.onkeyup=this.SoltarTecla;
-    }
+  var iniciar = function () {
+    document.onkeypress=IngresoDatos;
+    document.onkeyup=SoltarTecla;
+  };
+  return {
+    iniciar : iniciar,
   }
-};
+})();
 /*Inicializando el módulo "Calculadora"*/
 Calculadora.iniciar();
